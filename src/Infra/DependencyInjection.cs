2cb1e1296df0fd7fff96502e48f0d1f0644c90c6
fabio lima <fabio.lima19997@gmail.com>
2024-07-27 23:30:47 -0300

    change bd sqlite to postgre

--- src/Infra/DependencyInjection.cs
+++ src/Infra/DependencyInjection.cs
@@ -28,7 +28,7 @@ namespace IdentityUser.src.Infra
         {
             builder.Services.AddDbContext<AppDbContext>(options =>
             {
-                options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+                options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSQLConnection"));
             });
         }
 
